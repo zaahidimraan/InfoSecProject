798049be83389952b3e35ed5ee6e4910a6a4f84a
Zahid <i200469@nu.edu.pk>
2023-12-04 02:31:10 +0500

    things working perfectly

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, url_for, session, abort
+from flask_limiter import Limiter
 
 app = Flask(__name__)
 app.secret_key = '123'
@@ -7,13 +8,14 @@ app.secret_key = '123'
 def verify_user(code):
     return code == "1234"
 
+
 @app.before_request
 def before_request():
     # Enforce HTTPS for the internal page
-    if request.path == '/internal' and not request.is_secure and not request.headers.get('X-Forwarded-Proto', '').startswith('https') and not session.get('authenticated'):
+    if request.path == '/internal' and request.is_secure and session.get('authenticated'):
         abort(403)  # Forbidden
     # IP address filtering
-    if request.remote_addr != '127.0.0.1':
+    if request.remote_addr == '127.0.0.1' and request.path == '/internal':
         abort(403)  # Forbidden
     # Content type checking for POST requests
     if request.method == 'POST' and request.content_type != 'application/x-www-form-urlencoded':
@@ -23,15 +25,16 @@ def before_request():
 def externalnetwork():
     session['authenticated'] = False    # Set to True if you want to skip authentication
     return '''
-        Welcome to the homepage! You can access this page without authentication.
+        <h1>External Network</h1>
+        <p>Welcome to the external network! You can see this page without authenticating.</p>
         <br>
-        <a href="/server"><button>Go to Server Page</button></a>
+        <a href="/dmz"><button>Go to DMZ Layer</button></a>
         <br>
-        <a href="/internal"><button>Go to Internal Page</button></a>
+        <a href="/internal"><button>Go to Internal Layer</button></a>
     '''
 
-@app.route('/server', methods=['GET', 'POST'])
-def server():
+@app.route('/dmz', methods=['GET', 'POST'])
+def dmz():
     if request.method == 'POST':
         code = request.form.get('code')
         if verify_user(code):
@@ -40,6 +43,7 @@ def server():
         else:
             return redirect(url_for('internal'))
     return '''
+        <h1>DMZ Layer</h1>
         <form method="POST">
             Code: <input type="text" name="code">
             <input type="submit" value="Submit">
@@ -49,8 +53,13 @@ def server():
 @app.route('/internal')
 def internal():
     if not session.get('authenticated'):
-        return redirect(url_for('server'))
-    return "Welcome to the internal system! You can only see this page if you're authenticated."
+        return redirect(url_for('dmz'))
+    else:
+        session['authenticated'] = False
+    return  '''
+            <h1>Internal Page</h1>
+            <p>Welcome to the internal system! You can only see this page if you're authenticated.</p>
+            '''
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True, host='127.0.0.1' )
